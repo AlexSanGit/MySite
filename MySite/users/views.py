a81c410918e46b8cfeecd9a7198069a611bcf223
AlexSanGit <alexandrobabenko@gmail.com>
2023-06-05 11:32:21 +0600

    заработали шаблоны cryspi

--- MySite/users/views.py
+++ MySite/users/views.py
@@ -11,7 +11,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Ваш аккаунт создан: можно войти на сайт.')
-            return redirect('login')
+            return redirect('home')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
